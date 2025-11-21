3badeed06ff88d754f57c198167892420192f371
Andrei Solntsev <andrei.solntsev@gmail.com>
2025-11-21 08:35:56 +0200

    upgrade Spring Boot from 3.5.7 to 4.0.0

--- src/test/java/bigapp/calc/Calculator1111111Test.java
+++ src/test/java/bigapp/calc/Calculator1111111Test.java
@@ -3,8 +3,8 @@ package bigapp.calc;
 import bigapp.services.CalculatorService7;
 import org.junit.jupiter.api.RepeatedTest;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.webmvc.test.autoconfigure.AutoConfigureMockMvc;
+import org.springframework.boot.webmvc.test.autoconfigure.WebMvcTest;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.test.web.servlet.MockMvc;
 
--- src/test/java/bigapp/calc/Calculator11111Test.java
+++ src/test/java/bigapp/calc/Calculator11111Test.java
@@ -4,8 +4,8 @@ import bigapp.services.CalculatorService5;
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.RepeatedTest;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.webmvc.test.autoconfigure.AutoConfigureMockMvc;
+import org.springframework.boot.webmvc.test.autoconfigure.WebMvcTest;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.test.web.servlet.MockMvc;
 
